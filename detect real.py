f19bc5ae4d6a70828cd58d5fb6afdd793b0ffadc
JustGiy <gravifarhan@gmail.com>
2025-03-18 13:03:18 +0700

    update yolov5

--- detect real.py
+++ detect real.py
@@ -225,8 +225,7 @@ def run(
                 if not file_exists:
                     writer.writeheader()
                 writer.writerow(data)
-        def turn_on_light():
-            print("Emergency vehicle detected! Turning on the light.")
+
         # Process predictions
         for i, det in enumerate(pred):  # per image
             seen += 1
@@ -258,8 +257,7 @@ def run(
                     label = names[c] if hide_conf else f"{names[c]}"
                     confidence = float(conf)
                     confidence_str = f"{confidence:.2f}"
-                    if label == 'Emergency Vehicle' and confidence > 0.6:  # Confidence threshold
-                         turn_on_light()  # Jalankan fungsi tambahan
+
                     if save_csv:
                         write_to_csv(p.name, label, confidence_str)
 
@@ -313,8 +311,9 @@ def run(
                     vid_writer[i].write(im0)
 
         # Print time (inference-only)
-        # LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1e3:.1f}ms")
-        
+        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1e3:.1f}ms")
+        if label == 'Emergency Vehicle' and conf > 0.6:
+                        print("Emergency vehicle detected! Turning on the light.")
     # Print results
     t = tuple(x.t / seen * 1e3 for x in dt)  # speeds per image
     LOGGER.info(f"Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}" % t)
